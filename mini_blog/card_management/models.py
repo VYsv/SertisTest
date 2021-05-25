42c440f231d8315814462fde0a9b95bc94f3aaa7
VYsv <vee_535@hotmail.com>
2021-05-25 09:34:29 +0700

    Add card model

--- mini_blog/card_management/models.py
+++ mini_blog/card_management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.AutoField()
+    content = models.CharField(max_length=500)
+    category = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)

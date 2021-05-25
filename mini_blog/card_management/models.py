b76091e818cb1fd5c732653d1b04f402b9944203
VYsv <vee_535@hotmail.com>
2021-05-25 10:16:58 +0700

    Change status field to char field

--- mini_blog/card_management/models.py
+++ mini_blog/card_management/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Card(models.Model):
     name = models.CharField(max_length=100)
-    status = models.AutoField()
+    status = models.CharField(max_length=100)
     content = models.CharField(max_length=500)
     category = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
